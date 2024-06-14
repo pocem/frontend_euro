d276fdf6333f783771b519a4f3e08b2bbe476e5a
pocem <misogrega21@gmail.com>
2024-06-14 22:56:39 +0200

    deploy

--- React/server.js
+++ React/server.js
@@ -1,5 +1,10 @@
 import express from "express";
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const staticPath = path.join(__dirname, 'dist');
